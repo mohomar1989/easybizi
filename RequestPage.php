5d544f5735b42f38ac5943daf498c659256d92fb
z10188 <z10188@dm-z10188>
2018-03-30 01:05:33 +0400

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

--- RequestPage.php
+++ RequestPage.php
@@ -15,6 +15,11 @@ and open the template in the editor.
     <body>
 
         <div class="container col-12">
+
+
+
+
+
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 
                 <a class="navbar-brand"><img class="img-fluid" src="https://upload.wikimedia.org/wikipedia/commons/2/27/Square_200x200.svg" width="50" height="50"/></a>
@@ -35,36 +40,121 @@ and open the template in the editor.
                         <li class="nav-item">
                             <a class="nav-link" href="RequestPage.php">Requests Page</a>
                         </li>
-                       
+
                     </ul>
-                      <ul class="navbar-nav ml-auto">
-                     <li  class="nav-item ">
+                    <ul class="navbar-nav ml-auto">
+                        <li  class="nav-item ">
                             <a class="nav-link" href="#">Logout</a>
                         </li>
-                      </ul>
+                    </ul>
 
                 </div>
 
             </nav>
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item active" aria-current="page">RequestPage</li>
+                    <li class="breadcrumb-item active" aria-current="page">Request Page</li>
                 </ol>
             </nav>
 
-            
+         
+
+            <div class="card mt-2">
+
+                <div class="card-header"> Status Information</div>
+                <div class="card-body">
 
 
-            <div class="row">
-                
-                    
-                    
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label class="text-primary">Profile Status : Pending for approval</label>
+                        </div>
+
+                    </div>
+
+
+                </div>
             </div>
 
-                    
-               
+
+            <div class="card mt-2">
+
+                <div class="card-header"> Product Information Status</div>
+                <div class="card-body">
+
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr class="table-primary">
+                                        <th scope="col">#</th>
+                                        <th scope="col">Product Title</th>
+
+                                        <th scope="col">Status</th>
+                                        <th scope="col">Remove product</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+
+                                        <td>@mdo</td>
+                                        <td>
+                                            <button>delete</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+
+                                        <td>@fat</td>
+                                        <td>
+                                            <button>delete</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Larry the Bird</td>
+
+                                        <td>wew</td>
+                                        <td>
+                                            <button>delete</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+
+
+                </div>
             </div>
+
+
+            <nav aria-label="Page navigation example">
+                <ul class="pagination mt-2">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
+
 
 
 
