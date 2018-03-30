32d388eb1175f2458e0f8143ab6cfef004911a0a
mohomar1989 <mohammed.omar@appsolutesolutions.ae>
2018-03-30 14:56:18 +0400

    Delete dbConnect.php

--- mobApi/dbConnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = "localhost";
-$username = "j0qvbg2o_admin";
-$password = "Intheend11!";
-$dbName = "j0qvbg2o_EASY_BIZI";
-
-$link = mysqli_connect($servername, $username, $password, $dbName);
-mysqli_set_charset($link, "utf8");
