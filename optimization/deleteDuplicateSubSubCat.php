c4913d985080cd426151c6a05ae0a2792fc59d90
mohammedomar <mohammedomar@192.168.1.127>
2018-04-02 01:24:34 +0400

    Revert "new branch"
    This reverts commit 4582c63a5e14c59f7425daec81c63f53fc9231f2.

--- optimization/deleteDuplicateSubSubCat.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include ('../api/dbConnect.php');
-
-$query = "DELETE s1 FROM BUSINESS_SUBSUBCATEGORY s1 "
-        ."INNER JOIN BUSINESS_SUBSUBCATEGORY s2 "
-        . "WHERE s1.BusinessId = s2.BusinessId and s1.SubSubCategoryId = s2.SubSubCategoryId and s1.Id>s2.Id";
-echo $query;
-mysqli_query($link, $query);
